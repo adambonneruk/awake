2473d9d58e056ff7987654fb07807bdffa93699e
adambonneruk <adam@bonner.uk>
2020-09-16 14:46:18 +0100

    cleaned/linted code, added comments

--- wiggle.py
+++ wiggle.py
@@ -1,17 +1,25 @@
-import ctypes, time, datetime
+""" wiggle, small python program for windows used to wiggle the mouse and prevent screen lock/sleep
+"""
+import time
+import ctypes
 
+#Set Constants
 mouse_event = ctypes.windll.user32.mouse_event
 MOUSEEVENTF_MOVE = 0x0001
-print("press ctrl-c to end mouse shaker")
+
+#Display Message
+print("Wiggle, wiggle...")
+
+#Execute Main Loop
 try:
     while True:
-        mouse_event(MOUSEEVENTF_MOVE,25,0,0,0)
+        mouse_event(MOUSEEVENTF_MOVE, 32, 0, 0, 0)
         time.sleep(1)
-        mouse_event(MOUSEEVENTF_MOVE,0,25,0,0)
+        mouse_event(MOUSEEVENTF_MOVE, 0, 32, 0, 0)
         time.sleep(1)
-        mouse_event(MOUSEEVENTF_MOVE,-25,0,0,0)
+        mouse_event(MOUSEEVENTF_MOVE, -32, 0, 0, 0)
         time.sleep(1)
-        mouse_event(MOUSEEVENTF_MOVE,0,-25,0,0)
+        mouse_event(MOUSEEVENTF_MOVE, 0, -32, 0, 0)
         time.sleep(1)
 except KeyboardInterrupt:
     pass
