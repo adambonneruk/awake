394cf92a24a86e285895f87d7919b01c54ae1725
adambonneruk <adam@bonner.uk>
2023-08-09 10:21:34 +0100

    removed threading, time, and pyautogui

--- src/awake.py
+++ src/awake.py
@@ -1,129 +1,62 @@
-"""awake: a small utility to keep a windows machine from going asleep"""
-import threading, time, logging, pyautogui
-from pyautogui import press
-from pystray import MenuItem as item
-import pystray
-from PIL import Image
+"""awake: prevent the system from entering sleep or turning off the display"""
+import logging, pystray, os, ctypes
 import tkinter as tk
-import os
+from pystray import MenuItem as item
+from PIL import Image
 
-# Enabled/Disable Debug Mode
 DEBUGMODE = True
 if DEBUGMODE:
     logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-    logging.debug("DEBUG MODE ACTIVE")
-
-# Global Varibles
-tick = 0.4 # is 400ms
-toggling = True # we start the program actively toggling
-og_scroll_state = 0
-
-def get_scrolllock_state():
-    import ctypes
-    hllDll = ctypes.WinDLL ("User32.dll")
-    VK_SCROLL = 0x91 #https://learn.microsoft.com/en-gb/windows/win32/inputdev/virtual-key-codes?redirectedfrom=MSDN
-    return hllDll.GetKeyState(VK_SCROLL)
-
-def thread_toggle_scrolllock():
-    """handles launching of a second thread"""
-    global toggling, tick, og_scroll_state
-    logging.debug("2nd Thread Start")
-    pyautogui.FAILSAFE = False # fixes thread dying when cursor corner of screen
-    while getattr(threading.current_thread(), "do_run", True):
-        if toggling:
-            time.sleep(tick) 
-            logging.debug("toggling: " + str(toggling))
-            press('scrolllock')
-        else:
-            # if state is not og, return scroll lock state to original by press scroll lock key
-            time.sleep(tick)
-            logging.debug("not toggling: " + str(toggling))
-            if og_scroll_state != get_scrolllock_state():
-                press('scrolllock')
-    logging.debug("2nd Thread Stop")
+    logging.debug("DEBUGMODE = True")
 
 def main():
-    """main gui thread"""
-    global toggling
-    global og_scroll_state
+    """The main() function creates a window, a tray icon, and sets the execution state"""
 
-    # get scrollock state (so we can restore later)
-    logging.debug("original scroll state:")
-    og_scroll_state = get_scrolllock_state()
-    logging.debug(og_scroll_state)
-    print(og_scroll_state)
+    # 0x80000000 | ES_CONTINUOUS       | Informs the system that the state being set should remain in effect
+    # 0x00000002 | ES_DISPLAY_REQUIRED | Forces the display to be on by resetting the display idle timer
+    ctypes.windll.kernel32.SetThreadExecutionState(0x80000002)
+    logging.debug("SetThreadExecutionState = 0x80000002")
 
     # set the base directory for executable portability
     basedir = os.path.dirname(__file__)
 
-    # Create the Window
+    # create a window and load an image file
     window = tk.Tk()
     window.wm_attributes("-topmost", 1) #always on top
     window.resizable(0, 0) #no maximise button
-    window.title("Stay Awake, Don't Sleep!")
+    window.title("Awake")
     window.iconbitmap(os.path.join(basedir, "assets\\awake.ico"))
     window.geometry("512x256")
-
-    # Add an Image
     img = tk.PhotoImage(file=os.path.join(basedir, "assets\\ZZZZ.png"))
     img_label = tk.Label(window, image=img)
     img_label.pack()
 
-    # Launch Second (Scroll Lock Button Pressing) Thread
-    logging.debug("toggling: " + str(toggling))
-    thread = threading.Thread(target=thread_toggle_scrolllock)
-    thread.start()
-
     # Functions used for tray icon code
     def quit_window(icon, item):
+        """Stop the window, kill the process, this will trigger end of window.mainloop()"""
         icon.stop()
         window.destroy()
 
     def show_window(icon, item):
+        """Stop the tray icon/menu and relaunch the window"""
         icon.stop()
         window.after(0,window.deiconify)
 
-    def stop_or_start():
-        logging.debug("pause functionality trigger")
-        global toggling
-        toggling = not toggling # flip true to false or vice-versa
-
     def withdraw_window():
+        """Run the application in the tray instead of a window, build the icon and menu"""
         window.withdraw()
         image = Image.open(os.path.join(basedir, "assets\\awake-icon-64.png"))
-        menu = (item('Show', show_window), item("Start/Stop", stop_or_start), item('Quit', quit_window))
-        icon = pystray.Icon("name", image, "Stay Awake, Don't Sleep!", menu)
+        menu = (item('Show', show_window), item('Quit', quit_window))
+        icon = pystray.Icon("name", image, "prevent the system from entering sleep", menu)
         icon.run()
 
-    #Close to Tray
-    window.protocol("WM_DELETE_WINDOW", withdraw_window)
+    window.protocol("WM_DELETE_WINDOW", withdraw_window) # withdraw window instead of exit on close
+    withdraw_window() # withdraw the window (start in the tray)
+    window.mainloop() # start the main tkinter loop
 
-    #Start in Tray
-    withdraw_window()
-
-    #Main tkinter window loop
-    window.mainloop()
-
-    #Kill Second Thread
-    toggling = False
-    logging.debug(toggling)
-    thread.do_run = False
-    thread.join()
-
-    # if state is not og, return scroll lock state to original by press scroll lock key
-    print("wait 2 sec")
-    time.sleep(2)
-    print("og")
-    print(og_scroll_state)
-    print("current post 2nd thread")
-    print(get_scrolllock_state())
-    if str(og_scroll_state) != str(get_scrolllock_state()):
-        print("in the if")
-        press('scrolllock')
-    print("end of prog")
-    print("wait 2")
-    time.sleep(2)
-    print(get_scrolllock_state())
+    # now that the window.mainloop() has finished, close the program
+    ctypes.windll.kernel32.SetThreadExecutionState(0x80000000) # return to normal
+    logging.debug("SetThreadExecutionState = 0x80000000")
 
 if __name__ == "__main__":
     main()
