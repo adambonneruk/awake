3d7c8ddd06c5f5db8f2b1adca1546b769042b485
adambonneruk <adam@bonner.uk>
2023-08-15 15:43:01 +0100

    tool cleanup

--- tools/make.py
+++ tools/make.py
@@ -1,3 +1,15 @@
+""" make.py is a tool script used to...
+	(1) compile the solution with PyInstaller
+    (2) package this with NSIS
+"""
+
 import os
+
+print("make.py: compile and package installer")
+# compile solution, overwrite existing files (in build/dist folders) using predefined .spec file
+print("\n----------------------\npyinstaller src/awake.spec --noconfirm")
 os.system("pyinstaller src/awake.spec --noconfirm")
+
+# use nsis to build standard windows looking installer (and uninstaller)
+print("\n----------------------\nmakensis installer/awake.nsi")
 os.system("makensis installer/awake.nsi")
--- tools/press-scroll-lock.py
+++
@@ -1,3 +0,0 @@
-# my laptop doesn't have this key, so if i get stuck i can run this script to toggle scroll lock on/off
-from pyautogui import press
-press('scrolllock')
